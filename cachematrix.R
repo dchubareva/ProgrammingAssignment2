573683aba99b31134bbfad744b565b92d876e60c
chubarevadina <chubareva.dina@gmail.com>
2015-01-16 12:51:50 -0800

    my version of cachedMatrix.R with comments inside

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I created two functions that make inverted matrix without re-counting the same things (with cache option).
+## It works in 2 main steps: 
+##   - create a special object from your matrix (or redefine with set method)
+##     > myMatrix <- makeCacheMatrix(m), where m is your matrix
+##   - calculate inverted matrix and use cached results as much as you want
+##     > cacheSolve(myMatrix)
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## function creates a special object from matrix, which has additional functions to set or get inverted matrix (IM), and to set/get value of your "real" matrix
+ 
+makeCacheMatrix<- function(x = matrix()) {
+  invX <- NULL
+  set <- function(y) {
+    x <<- y
+    invX <<- NULL
+  }
+  get <- function() x
+  setinvert <- function(givenMr) invX <<- givenMr
+  getinvert <- function() invX
+  list(set = set, get = get,
+       setinvert = setinvert,
+       getinvert = getinvert)
+  
 }
 
-
-## Write a short comment describing this function
+## function checks whether IM is already calculated or not. If it is calculated, function returns IM and makes text-comment about it. If not, it calculates IM and stores it in makeCacheMatrix-object
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinvert()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinvert(inv)
+  inv
 }
